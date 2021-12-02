f282f097d7cff6fcfef622fd272bc267a8096e89
Reilly Moore <reilly@knaque.dev>
2021-12-02 10:46:24 -0600

    pretty

--- 01/part1.nim
+++ 01/part1.nim
@@ -1,5 +1,16 @@
 import strutils, sequtils
-var (c, l) = (0, readFile("input.txt").strip().splitLines().mapIt(parseInt(it)))
+
+var l = readFile("input.txt")
+  .strip()
+  .splitLines()
+  .mapIt(
+    parseInt(it)
+  )
+
+var c = 0
+
 for i, v in l[1..^1]:
-  if l[i] < v: inc(c)
+  if l[i] < v:
+    inc c
+
 echo c
--- 01/part2.nim
+++ 01/part2.nim
@@ -1,5 +1,18 @@
 import strutils, sequtils, math
-var (c, l) = (0, readFile("input.txt").strip().splitLines().mapIt(parseInt(it)))
+
+var l = readFile("input.txt")
+  .strip()
+  .splitLines()
+  .mapIt(
+    parseInt(it)
+  )
+
+var c = 0
+
 for i, _ in l[0..^4]:
-  if sum(l[i..i+2]) < sum(l[i+1..i+3]): inc(c)
+  var a = sum(l[i..i+2])
+  var b = sum(l[i+1..i+3])
+  if a < b:
+    inc c
+
 echo c
