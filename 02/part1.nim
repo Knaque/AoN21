8a23c1d8675e0016df6bf7ef16d8041aa08f86e7
Reilly Moore <reilly@knaque.dev>
2021-12-02 10:46:38 -0600

    pretty

--- 02/part1.nim
+++ 02/part1.nim
@@ -1,13 +1,26 @@
 import strutils, sequtils
-var (h, d, l) = (0, 0,
-  readFile("input.txt").strip().splitLines().mapIt(
-    (d: it.split(" ")[0], v: it.split(" ")[1].parseInt())
+
+var l = readFile("input.txt")
+  .strip()
+  .splitLines()
+  .mapIt(
+    (
+      d: it.split(" ")[0],
+      v: it.split(" ")[1].parseInt()
+    )
   )
-)
+
+var (h, d) = (0, 0)
+
 for m in l:
   case m.d
-  of "forward": h += m.v
-  of "down": d += m.v
-  of "up": d -= m.v
-  else: discard
+  of "forward":
+    h += m.v
+  of "down":
+    d += m.v
+  of "up":
+    d -= m.v
+  else:
+    discard
+
 echo h * d
--- 02/part2.nim
+++ 02/part2.nim
@@ -1,13 +1,27 @@
 import strutils, sequtils
-var (h, d, a, l) = (0, 0, 0,
-  readFile("input.txt").strip().splitLines().mapIt(
-    (d: it.split(" ")[0], v: it.split(" ")[1].parseInt())
+
+var l = readFile("input.txt")
+  .strip()
+  .splitLines()
+  .mapIt(
+    (
+      d: it.split(" ")[0],
+      v: it.split(" ")[1].parseInt()
+    )
   )
-)
+
+var (h, d, a) = (0, 0, 0)
+
 for m in l:
   case m.d
-  of "forward": h += m.v; d += m.v * a
-  of "down": a += m.v
-  of "up": a -= m.v
-  else: discard
+  of "forward":
+    h += m.v
+    d += m.v * a
+  of "down":
+    a += m.v
+  of "up":
+    a -= m.v
+  else:
+    discard
+
 echo h * d
